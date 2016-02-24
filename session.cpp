b447ea5ddc2bd5b57dfd7d75c0672b0d6ee3cff6
Ну понятно... ture <eturin@gmail.com>
2016-02-24 01:56:13 -0500

    te

--- session.cpp
+++ session.cpp
@@ -1,6 +1,9 @@
 #include "session.h"
 #include <stdio.h>
+#include <unistd.h>
 #include <iostream>
+#include <errno.h>
+#include <cstring>
 
 Session & Session::operator<<(const char * buf){
  	msg+=buf;		
@@ -13,16 +16,20 @@ std::string Session::get_response()const{
 	res="HTTP/1.0 404 NOT FOUND\r\n"
 	    "Content-Type: text/html\r\n"
 	    "\r\n";
-	//std::cout<<msg<<std::endl;
-	/*определяем метод*/
+	std::cout<<msg<<std::endl;
 	size_t pos=msg.find(' ',0);
+	/*определяем метод*/
 	if(pos>0 && "GET"==msg.substr(0,pos)){
 		/*определяем запрашиваемый файл*/
 		size_t posf=msg.find(' ',pos+1);	
-		std::string path="."+msg.substr(pos+1,posf-pos-1);//т.к. мы в каталоге www, то добавим току, для ссылки на текущий каталог
-		path=path.substr(0,path.find('?',0));//отрежем все параметры, т.к. не обрабатываются они
+		std::string path=msg.substr(pos+1+1,posf-pos-1-1);//т.к. мы в каталоге www, то добавим точку, для ссылки на текущий каталог
+		pos=path.find('?',0);
+		if(pos!=-1)
+			path=path.substr(0,path.find('?',0));//отрежем все параметры, т.к. не обрабатываются они
+	//	std::cout<<"Окончательный путь path = \""<<path<<"\""<<std::endl;
+
 		/*проверяем наличие файла*/
-		if(path[path.length()-1]!='/'){
+		if(path.length() && path[path.length()-1]!='/'){
 			FILE * file=fopen(path.c_str(),"r");
 			if(file!=NULL){
 				int n,len=0;
@@ -41,9 +48,13 @@ std::string Session::get_response()const{
 	    		    	     "Content-Type: text/html\r\n"
 	    		     	     "\r\n";
 				res+=res_;
-			}
+			}/*else{
+				char buf[512];
+				getcwd(buf,512);
+				std::cout<<"Не удалось открыть на чтение файл:\n"<<strerror(errno)<<std::endl<<buf<<std::endl;
+			}*/
 		}
 	}
-	//std::cout<<res<<std::endl;
+//	std::cout<<res<<std::endl;
 	return res;
 }
